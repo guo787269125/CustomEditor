402176a2cdb8e3a3e0dd3ebccafb96b4c0c6f69a
DAWA010\Mantra <guo_787269125@hotmail.com>
2018-08-18 20:33:00 +0800

    添加了创建CustomPin所必须的基本类CustomAsset FConstructPinFactory和SCustomGraphPin

---
+++ Plugins/MantraCustomPin/Source/MantraCustomPin/Public/CustomAsset.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "CustomAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MANTRACUSTOMPIN_API UCustomAsset : public UObject
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
