0c44ffb4976f4c236e9327ea3cf8551437094060
DAWA010\Mantra <guo_787269125@hotmail.com>
2018-08-18 20:21:27 +0800

    添加了自定义ConsoleCommand的功能（以插件的形式存在）

---
+++ Plugins/CustomConsoleCommand/Source/CustomConsoleCommand/Private/CustomConsoleCommand.cpp
@@ -0,0 +1,32 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "CustomConsoleCommand.h"
+#include <IConsoleManager.h>
+
+#define LOCTEXT_NAMESPACE "FCustomConsoleCommandModule"
+
+void FCustomConsoleCommandModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+
+	// ¥¥Ω®√¸¡Ó≤¢∞Û∂®(√¸¡Ó√˚≥∆°¢√¸¡Ó∞Ô÷˙°¢√¸¡ÓŒØÕ–)
+	DisplayTestCommand = IConsoleManager::Get().RegisterConsoleCommand(TEXT("Display Custom Command"), 
+		TEXT("This is a custom command created by Mantra"),
+		FConsoleCommandDelegate::CreateRaw(this, &FCustomConsoleCommandModule::DisplayWindow, FString("Test Command Window"))
+		);
+}
+
+void FCustomConsoleCommandModule::ShutdownModule()
+{
+	// –∂‘ÿ√¸¡Ó
+	if (DisplayTestCommand) IConsoleManager::Get().UnregisterConsoleObject(DisplayTestCommand);
+}
+
+void FCustomConsoleCommandModule::DisplayWindow(FString WindoeTitle)
+{
+	UE_LOG(LogTemp, Warning, TEXT("This ia a custom window command result"));
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FCustomConsoleCommandModule, CustomConsoleCommand)
